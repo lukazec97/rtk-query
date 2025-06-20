a29bbc4bd9b46d95504e0afb73dd5d4fcc0923d5
lukazec97 <38230751+lukazec97@users.noreply.github.com>
2025-06-20 14:17:47 +0200

    Create jsonPlaceholderApi.js

---
+++ src/services/jsonPlaceholderApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const jsonPlaceholderApi = createApi({
+  reducerPath: "jsonPlaceholderApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com",
+  }),
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: ({limit, offset}) => `/posts?_limit=${limit}&_offset=${offset}`,
+    }),
+    createPost: builder.mutation({
+      query: (newPost) => ({
+        url: "/posts",
+        method: "POST",
+        body: newPost,
+      }),
+    }),
+  }),
+});
+
+export const { useGetPostsQuery, useCreatePostMutation } = jsonPlaceholderApi;
