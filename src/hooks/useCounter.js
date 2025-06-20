96d426a77cf89001d743c936b50e9c729a35c476
lukazec97 <38230751+lukazec97@users.noreply.github.com>
2025-06-20 14:15:01 +0200

    Create useCounter.js

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+export const useCounter = (initialValue = 0) => {
+  const [count, setCount] = useState(initialValue);
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+  return { count, increment, decrement };
+};
